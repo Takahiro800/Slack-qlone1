a04229d86845cf96bf770675c8cf6f65141c3bbc
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-02-24 16:29:35 +0900

    default.vueによる大枠の作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <nuxt />
+  <div class = "app-layout">
+    <div class = "sidebar">
+    </div>
+    <div class = "main-content">
+      <nuxt />
+    </div>
   </div>
 </template>
 
@@ -51,5 +55,21 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+
+.app-layout {
+  display: flex;
+}
+
+.sidebar{
+  width: 300px;
+  background: #4A4141;
+  height: 100vh;
+}
+
+.main-content{
+  width: 100%;
+  background: #F1F1F1;
+  height: 100vh;
 }
 </style>
