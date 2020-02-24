f92f9c46e54aa9331636f2df3ff336dbc32788bc
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-02-24 16:35:59 +0900

    チャンネル一覧の作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <div class = "app-layout">
     <div class = "sidebar">
+      <p>チャンネル一覧</p>
+      <p>#general</p>
+      <p>#random</p>
+      <p>#random</p>
     </div>
     <div class = "main-content">
       <nuxt />
@@ -65,6 +69,12 @@ html {
   width: 300px;
   background: #4A4141;
   height: 100vh;
+  padding: 20px;
+}
+
+.sidebar p{
+  color: #DDDDDD;
+  padding-top: 4px;
 }
 
 .main-content{
