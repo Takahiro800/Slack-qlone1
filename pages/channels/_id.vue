7decb8e41aa30153f8e54f6d8b7c542bc5374a43
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-03-06 05:00:28 +0900

    チャットルームへの移動機能を実装

--- pages/channels/_id.vue
+++ pages/channels/_id.vue
@@ -1,12 +1,17 @@
 <template>
  <div class="container">
+   <div class="chats-layout">
+     <messages />
+   </div>
+   <div class="input-layout">
+     <chat-form />
+   </div>
  </div>
 </template>
 
 <script>
 import Messages from '~/components/Messages.vue'
 import ChatForm from '~/components/ChatForm.vue'
-import { db } from '~/plugins/firebase'
 
 export default {
  components: {
@@ -29,5 +34,4 @@ export default {
 .input-layout {
  height: 10%;
 }
-
 </style>
